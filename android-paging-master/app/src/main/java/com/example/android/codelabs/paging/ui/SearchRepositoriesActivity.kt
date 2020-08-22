bd0ab317e4f248989a97a21354900bc607487d91
HieuTrinh <hieu.trinh@student.tut.fi>
2020-08-22 23:43:35 +0300

    add sample 'paging'

---
+++ android-paging-master/app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesActivity.kt
@@ -0,0 +1,208 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.codelabs.paging.ui
+
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.ExperimentalPagingApi
+import kotlinx.coroutines.flow.collect
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
+import com.example.android.codelabs.paging.Injection
+import com.example.android.codelabs.paging.databinding.ActivitySearchRepositoriesBinding
+import com.example.android.codelabs.paging.model.RepoSearchResult
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChangedBy
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.launch
+
+@ExperimentalCoroutinesApi
+class SearchRepositoriesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchRepositoriesBinding
+    private lateinit var viewModel: SearchRepositoriesViewModel
+    private val adapter = ReposAdapter()
+
+    private var searchJob: Job? = null
+
+    private fun search(query: String) {
+        // Make sure we cancel the previous job before creating a new one
+        searchJob?.cancel()
+        searchJob = lifecycleScope.launch {
+            viewModel.searchRepo(query).collectLatest {
+                adapter.submitData(it)
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchRepositoriesBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // get the view model
+        viewModel = ViewModelProvider(this, Injection.provideViewModelFactory(this))
+                .get(SearchRepositoriesViewModel::class.java)
+
+        // add dividers between RecyclerView's row items
+        val decoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        binding.list.addItemDecoration(decoration)
+//        setupScrollListener()
+
+        initAdapter()
+        val query = savedInstanceState?.getString(LAST_SEARCH_QUERY) ?: DEFAULT_QUERY
+        search(query)
+//        if (viewModel.repoResult.value == null) {
+//            viewModel.searchRepo(query)
+//        }
+        initSearch(query)
+
+        binding.retryButton.setOnClickListener { adapter.retry() }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(LAST_SEARCH_QUERY, binding.searchRepo.text.trim().toString())
+    }
+
+    private fun initAdapter() {
+        binding.list.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = ReposLoadStateAdapter { adapter.retry() },
+                footer = ReposLoadStateAdapter { adapter.retry() }
+        )
+        adapter.addLoadStateListener { loadState ->
+            // Only show the list if refresh succeeds.
+            binding.list.isVisible = loadState.source.refresh is LoadState.NotLoading
+            // Show loading spinner during initial load or refresh.
+            binding.progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+            // Show the retry state if initial load or refresh fails.
+            binding.retryButton.isVisible = loadState.source.refresh is LoadState.Error
+
+            // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
+            val errorState = loadState.source.append as? LoadState.Error
+                    ?: loadState.source.prepend as? LoadState.Error
+                    ?: loadState.append as? LoadState.Error
+                    ?: loadState.prepend as? LoadState.Error
+            errorState?.let {
+                Toast.makeText(
+                        this,
+                        "\uD83D\uDE28 Wooops ${it.error}",
+                        Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+//        viewModel.repoResult.observe(this) { result ->
+//            when (result) {
+//                is RepoSearchResult.Success -> {
+//                    showEmptyList(result.data.isEmpty())
+//                    adapter.submitList(result.data)
+//                }
+//                is RepoSearchResult.Error -> {
+//                    Toast.makeText(
+//                            this,
+//                            "\uD83D\uDE28 Wooops $result.message}",
+//                            Toast.LENGTH_LONG
+//                    ).show()
+//                }
+//            }
+//        }
+    }
+
+    private fun initSearch(query: String) {
+        binding.searchRepo.setText(query)
+
+        binding.searchRepo.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                updateRepoListFromInput()
+                true
+            } else {
+                false
+            }
+        }
+        binding.searchRepo.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                updateRepoListFromInput()
+                true
+            } else {
+                false
+            }
+        }
+
+        // Scroll to top when the list is refreshed from network
+        lifecycleScope.launch {
+            adapter.loadStateFlow
+                    // Only emit when REFRESH LoadState changes.
+                    .distinctUntilChangedBy { it.refresh }
+                    // Only react to cases where REFRESH completes i.e., NotLoading.
+                    .filter { it.refresh is LoadState.NotLoading }
+                    .collect { binding.list.scrollToPosition(0) }
+        }
+    }
+
+    private fun updateRepoListFromInput() {
+        binding.searchRepo.text.trim().let {
+            if (it.isNotEmpty()) {
+                // 1
+//                viewModel.searchRepo(it.toString())
+//                binding.list.scrollToPosition(0)
+                search(it.toString())
+            }
+        }
+    }
+
+//    private fun showEmptyList(show: Boolean) {
+//        if (show) {
+//            binding.emptyList.visibility = View.VISIBLE
+//            binding.list.visibility = View.GONE
+//        } else {
+//            binding.emptyList.visibility = View.GONE
+//            binding.list.visibility = View.VISIBLE
+//        }
+//    }
+
+//    private fun setupScrollListener() {
+//        val layoutManager = binding.list.layoutManager as LinearLayoutManager
+//        binding.list.addOnScrollListener(object : OnScrollListener() {
+//            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+//                super.onScrolled(recyclerView, dx, dy)
+//                val totalItemCount = layoutManager.itemCount
+//                val visibleItemCount = layoutManager.childCount
+//                val lastVisibleItem = layoutManager.findLastVisibleItemPosition()
+//
+//                viewModel.listScrolled(visibleItemCount, lastVisibleItem, totalItemCount)
+//            }
+//        })
+//    }
+
+    companion object {
+        private const val LAST_SEARCH_QUERY: String = "last_search_query"
+        private const val DEFAULT_QUERY = "Android"
+    }
+}
---
+++ android-paging-master/app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesViewModel.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.codelabs.paging.ui
+
+import androidx.lifecycle.*
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.insertSeparators
+import androidx.paging.map
+import com.example.android.codelabs.paging.data.GithubRepository
+import com.example.android.codelabs.paging.model.Repo
+import com.example.android.codelabs.paging.model.RepoSearchResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+/**
+ * ViewModel for the [SearchRepositoriesActivity] screen.
+ * The ViewModel works with the [GithubRepository] to get the data.
+ */
+@ExperimentalCoroutinesApi
+class SearchRepositoriesViewModel(private val repository: GithubRepository) : ViewModel() {
+
+//    companion object {
+//        private const val VISIBLE_THRESHOLD = 5
+//    }
+//
+//    private val queryLiveData = MutableLiveData<String>()
+//    val repoResult: LiveData<RepoSearchResult> = queryLiveData.switchMap { queryString ->
+//        liveData {
+//            val repos = repository.getSearchResultStream(queryString).asLiveData(Dispatchers.Main)
+//            emitSource(repos)
+//        }
+//    }
+//
+//    /**
+//     * Search a repository based on a query string.
+//     */
+//    fun searchRepo(queryString: String) {
+//        queryLiveData.postValue(queryString)
+//    }
+//
+//    fun listScrolled(visibleItemCount: Int, lastVisibleItemPosition: Int, totalItemCount: Int) {
+//        if (visibleItemCount + lastVisibleItemPosition + VISIBLE_THRESHOLD >= totalItemCount) {
+//            val immutableQuery = queryLiveData.value
+//            if (immutableQuery != null) {
+//                viewModelScope.launch {
+//                    repository.requestMore(immutableQuery)
+//                }
+//            }
+//        }
+//    }
+    private var currentQueryValue: String? = null
+
+    private var currentSearchResult: Flow<PagingData<UiModel>>? = null
+
+    fun searchRepo(queryString: String): Flow<PagingData<UiModel>> {
+        val lastResult = currentSearchResult
+        if (queryString == currentQueryValue && lastResult != null) {
+            return lastResult
+        }
+        currentQueryValue = queryString
+        val newResult: Flow<PagingData<UiModel>> = repository.getSearchResultStream(queryString)
+                .map { pagingData -> pagingData.map { UiModel.RepoItem(it) } }
+                .map {
+                    it.insertSeparators<UiModel.RepoItem, UiModel> { before, after ->
+                        if (after == null) {
+                            // we're at the end of the list
+                            return@insertSeparators null
+                        }
+
+                        if (before == null) {
+                            // we're at the beginning of the list
+                            return@insertSeparators UiModel.SeparatorItem("${after.roundedStarCount}0.000+ stars")
+                        }
+                        // check between 2 items
+                        if (before.roundedStarCount > after.roundedStarCount) {
+                            if (after.roundedStarCount >= 1) {
+                                UiModel.SeparatorItem("${after.roundedStarCount}0.000+ stars")
+                            } else {
+                                UiModel.SeparatorItem("< 10.000+ stars")
+                            }
+                        } else {
+                            // no separator
+                            null
+                        }
+                    }
+                }
+                .cachedIn(viewModelScope)
+        currentSearchResult = newResult
+        return newResult
+    }
+}
+
+sealed class UiModel {
+    data class RepoItem(val repo: Repo): UiModel()
+    data class SeparatorItem(val description: String): UiModel()
+}
+
+private val UiModel.RepoItem.roundedStarCount: Int
+    get() = this.repo.stars / 10_000
